c3a00fc3cfb379ddc26c6ccc87a27eee695b4dca
TDsFlutter <trunaldungarani15@gmail.com>
2026-03-22 22:23:36 +0530

    Fix Vite config unused variable to pass tsc build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   base: '/crickboss-testweb/',
   plugins: [react()],
-}))
+})
